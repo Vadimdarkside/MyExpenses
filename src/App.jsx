7ddf66dfd329db15ac05e7759ae231fcab50696c
VadimBondar <ipz212_bvm@student.ztu.edu.ua>
2023-12-08 15:30:47 +0100

    few subtle improvments

--- src/App.jsx
+++ src/App.jsx
@@ -17,41 +17,17 @@ const SelectedCategory = lazy(() =>
 function App() {
   const [projectState, setProjectState] = useState({
     selectedCategoryId: undefined,
-    // categories: [],
-    // onLoad: true,
   });
-  const [data, setData, isDataLoading] = useLocalStorage(
+  const [categories, setCategories, isDataLoading] = useLocalStorage(
     "categories",
     CATEGORIES_DATA,
   );
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     const storedCategories =
-  //       JSON.parse(localStorage.getItem("categories")) || [];
-  //     if (storedCategories.length !== 0) {
-  //       setProjectState((prev) => {
-  //         return {
-  //           ...prev,
-  //           onLoad: false,
-  //           categories: storedCategories,
-  //         };
-  //       });
-  //     } else {
-  //       //любі друзі! Це муляж дати, коли допишете категорії, якшо допишете), треба буде прибрать
-  //       setProjectState((prev) => {
-  //         return {
-  //           ...prev,
-  //           onLoad: false,
-  //           categories: CATEGORIES_DATA,
-  //         };
-  //       });
-  //     }
-  //   }, 1000);
-  // }, []);
 
   const onCategoryDeleteHandler = (id) => {
-    const filteredCategories = data.filter((category) => category.id !== id);
-    setData(filteredCategories);
+    const filteredCategories = categories.filter(
+      (category) => category.id !== id,
+    );
+    setCategories(filteredCategories);
     setProjectState((prevState) => {
       return {
         ...prevState,
@@ -82,7 +58,7 @@ function App() {
   } else if (projectState.selectedCategoryId === null) {
     context = <NewCategory />;
   } else {
-    let category = data.find(
+    let category = categories.find(
       (item) => item.id === projectState.selectedCategoryId,
     );
     context = (
@@ -96,7 +72,7 @@ function App() {
     <main className="h-[100%] my-8 flex gap-2">
       <SideBar
         onLoad={isDataLoading}
-        categories={data}
+        categories={categories}
         onCategoryDelete={onCategoryDeleteHandler}
         onCreateCategory={onCreateCategoryHandler}
         onSelectCategory={onSelectCategoryHandler}
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 export default function useLocalStorage(key, initialValue) {
   const [data, setData] = useState([]);
   const [isDataLoading, setIsDataLoading] = useState(true);
-  const [timerFinished, setTimerFinished] = useState(false); //-this state only for 2second wait logic for data fetching
-
+  const [timerFinished, setTimerFinished] = useState(false); //!!!-this state only for 2second wait logic for data fetching, in prod - delete timer
+  //1
   useEffect(() => {
     setTimeout(() => {
       const storedData = localStorage.getItem(key);
@@ -13,9 +13,10 @@ export default function useLocalStorage(key, initialValue) {
       setTimerFinished(true);
     }, 2000);
   }, []);
-
+  //2
   useEffect(() => {
     if (timerFinished) {
+      //Це треба, бо через таймер в "1", код в цьому ефекті виконається першим, шо не треба
       localStorage.setItem(key, JSON.stringify(data));
     }
   }, [key, data]);
