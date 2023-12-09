d8013532d7bd7c3a097c94f2a7fa8cc3d6302f6e
VadimBondar <ipz212_bvm@student.ztu.edu.ua>
2023-12-09 01:40:03 +0100

    Modal fix for category - Only one Modal, instead a lot for every btn

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,11 +1,29 @@
 import CategoryBtn from "./CategoryBtn";
 import "./CategoryList.css";
+import { useEffect, useRef, useState } from "react";
 import { List, AutoSizer } from "react-virtualized";
+import DeleteCategoryModal from "./Modal/DeleteCategoryModal";
 export default function Categories({
   categories,
   onCategoryDelete,
   onSelectCategory,
 }) {
+  const [deleteId, setDeleteId] = useState(null);
+  const Modal = useRef();
+
+  const handleCategoryDelete = (id) => {
+    setDeleteId(id);
+  };
+
+  useEffect(() => {
+    //console.log(deleteId);
+    if (deleteId) {
+      // console.log("Open");
+      //console.log(deleteId);
+      Modal.current.Open();
+    }
+  });
+
   return (
     <>
       {categories.length == 0 && (
@@ -19,43 +37,41 @@ export default function Categories({
         </div>
       )}
       {categories.length > 0 && (
-        <AutoSizer>
-          {({ width }) => (
-            <List
-              width={width}
-              height={400}
-              rowHeight={100}
-              rowCount={categories.length}
-              rowRenderer={({ key, index, style, parent }) => {
-                const category = categories[index];
-                return (
-                  <CategoryBtn
-                    style={style}
-                    id={category.id}
-                    key={key}
-                    name={category.name}
-                    color={category.color}
-                    img={category.img}
-                    onCategoryDelete={onCategoryDelete}
-                    onSelectCategory={onSelectCategory}
-                  ></CategoryBtn>
-                );
-              }}
-            ></List>
-          )}
-        </AutoSizer>
+        <>
+          <DeleteCategoryModal
+            ref={Modal}
+            onApply={() => {
+              onCategoryDelete(deleteId);
+              setDeleteId(null);
+            }}
+          />
+          <AutoSizer>
+            {({ width }) => (
+              <List
+                width={width}
+                height={400}
+                rowHeight={100}
+                rowCount={categories.length}
+                rowRenderer={({ key, index, style, parent }) => {
+                  const category = categories[index];
+                  return (
+                    <CategoryBtn
+                      style={style}
+                      id={category.id}
+                      key={key}
+                      name={category.name}
+                      color={category.color}
+                      img={category.img}
+                      onCategoryDelete={handleCategoryDelete}
+                      onSelectCategory={onSelectCategory}
+                    ></CategoryBtn>
+                  );
+                }}
+              ></List>
+            )}
+          </AutoSizer>
+        </>
       )}
-      {/* {categories.map((item) => (
-        <CategoryBtn
-          id={item.id}
-          key={item.id}
-          name={item.name}
-          color={item.color}
-          img={item.img}
-          onCategoryDelete={onCategoryDelete}
-          onSelectCategory={onSelectCategory}
-        ></CategoryBtn>
-      ))} */}
     </>
   );
 }
