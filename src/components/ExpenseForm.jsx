96939af4b96de8e4a39fcdc06f4b387614ba4c18
Коваль Дмитро <fyntikminiz@gmail.com>
2023-12-20 19:00:13 +0200

    Added conditional rendering

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 export default function ExpenseForm({ onAddExpense }) {
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState('');
+  const [showForm, setShowForm] = useState(false);
 
   const submitHandler = (event) => {
     event.preventDefault();
     onAddExpense({ title, amount: Number(amount) });
     setTitle('');
     setAmount('');
+    setShowForm(false);
   };
 
   const formStyle = {
@@ -36,11 +38,37 @@ export default function ExpenseForm({ onAddExpense }) {
     cursor: 'pointer',
   };
 
+  const hideButtonStyle = {
+    padding: '10px 20px',
+    fontSize: '16px',
+    borderRadius: '5px',
+    border: 'none',
+    backgroundColor: 'white',
+    color: 'black',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translateX(-50%)'
+  };
+
+  const buttonWrapperStyle = {
+    position: 'relative',
+    height: '50px'
+  };
+
+  if (!showForm) {
+    return (
+      <div style={buttonWrapperStyle}>
+        <button style={hideButtonStyle} onClick={() => setShowForm(true)}>Show Form</button>
+      </div>
+    );
+  }
+
   return (
     <form onSubmit={submitHandler} style={formStyle}>
       <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Expense Title" required style={inputStyle} />
       <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Amount" required style={inputStyle} />
       <button type="submit" style={buttonStyle}>Add Expense</button>
+      <button type="button" style={{ ...buttonStyle, backgroundColor: 'white', color: 'black' }} onClick={() => setShowForm(false)}>Hide Form</button>
     </form>
   );
 }
